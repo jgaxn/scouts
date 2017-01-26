9609a1cd06c4d32dd4216fd3d46a90f0d2ceea9b
John Atkinson <John.Atkinson@mx.com>
2017-01-25 20:17:21 -0700

    deleted one line

--- password2.rb
+++ password2.rb
@@ -1,7 +1,6 @@
 def total(string)
   characters = string.split("")
   values = characters.map{|character| character.ord}
-  values.inject(0) {|sum, val| sum + val}
 
   total = 0
   values.each do |value|
